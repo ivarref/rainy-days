76e164aeeb2b022121caf97c6e893194859aaff1
Ivar Refsdal <refsdal.ivar@gmail.com>
2014-09-08 20:10:41 +0200

    o Save hourly data to DB.

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -1,4 +1,8 @@
 drop table rain;
+-- date format oracle : yyyy-MM-dd HH24:MI:SS
 
-create table rain (measure_time timestamp, rain number(15,5));
+create table rain (measure_time timestamp, rain number(15,5), quality number);
 
+select stddev(rain) from rain where rain > 0 order by measure_time asc;
+
+select * from rain order by rain desc;
--- src/main/scala/GetData.scala
+++ src/main/scala/GetData.scala
@@ -1,5 +1,5 @@
 import java.sql.{PreparedStatement, Timestamp}
-import java.text.SimpleDateFormat
+import java.text.{DateFormat, SimpleDateFormat}
 import java.util.Date
 
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
@@ -17,13 +17,19 @@ object GetData {
     config.setConnectionTimeout(1500)
     val ds: HikariDataSource = new HikariDataSource(config)
 
+    /*
     val from = "2006-01-01"
-    val to = "2006-12-31"
-    val element = "RR"
-
-    val data = io.Source.fromURL(s"http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=0&format=&from=${from}&to=${to}&stations=18700&elements=${element}&hours=&months=&username=").mkString
+    val to = "2006-01-01"
+    */
+    val from = "2014-01-01"
+    val to = "2014-09-08"
+    val element = "RR_1"
+    val hours = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23"
+    val url: String = s"http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=2&format=&from=${from}&to=${to}&stations=18700&elements=${element}&hours=${hours}&months=&username="
+    val data = io.Source.fromURL(url).mkString
     val xml: Elem = XML.loadString(data)
-
+    println("url is ")
+    println(url)
     //println(new PrettyPrinter(160, 1).format(xml))
 
     val content = xml \\ "Envelope" \ "Body" \ "getMetDataResponse" \ "return" \ "timeStamp" \ "item"
@@ -35,23 +41,25 @@ object GetData {
     content.foreach(child => {
       def prop(x: String) = { (child \\ x).text}
       val date: String = prop("from")
-      val value: String = if ("-1.0".equals(prop("value"))) "0.0" else prop("value")
+      var value: String = if ("-1.0".equals(prop("value"))) "0.0" else prop("value")
+      value = if ("-99999".equals(value)) "0.0" else value
+
       val quality: String = prop("quality")
       println(s"${date} = quality: ${quality}, value: ${value}")
-      if (!"2".equals(quality))
-        throw new RuntimeException("Bad quality: " + quality)
+      if (!("0,1,2,5".split(",").contains(quality)))
+        throw new RuntimeException("Unknown quality: " + quality)
 
-      val ps: PreparedStatement = conn.prepareStatement("insert into rain (measure_time, rain) values (?, ?)")
-      val dateParsed: Date = new SimpleDateFormat("yyyy-MM-dd").parse(date)
-      ps.setTimestamp(1, new Timestamp(dateParsed.getTime))
+      val fullDate: Date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(date)
+      val ps: PreparedStatement = conn.prepareStatement("insert into rain (measure_time, rain, quality) values (?, ?, ?)")
       val bd: BigDecimal = BigDecimal(value)
+      ps.setTimestamp(1, new Timestamp(fullDate.getTime))
       ps.setBigDecimal(2, bd.bigDecimal)
+      ps.setLong(3, quality.toLong)
       ps.executeUpdate()
       ps.close()
     })
 
     conn.commit()
     conn.close()
-
   }
 }
