0cc3312c7302e42ccc52f166844048f540debccf
Ivar Refsdal <refsdal.ivar@gmail.com>
2014-09-09 19:34:17 +0200

    o Add caching on local disk. o Data for Blindern is about 120 MiB for the period 1993 - 2014 (~188 000 measurements).

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -3,6 +3,13 @@ drop table rain;
 
 create table rain (measure_time timestamp, rain number(15,5), quality number);
 
-select stddev(rain) from rain where rain > 0 order by measure_time asc;
+select stddev(rain) from rain where rain > 0;
 
 select * from rain order by rain desc;
+
+-- 1993-01-04 13:00:00
+
+-- avg 0.9094451503600169419737399407030919102075
+-- stddev 1.47714994069103761022074064706227422142
+
+select avg(rain), stddev(rain) from rain;
--- src/main/scala/GetData.scala
+++ src/main/scala/GetData.scala
@@ -1,5 +1,7 @@
+import java.io.{File, PrintWriter}
+import java.security.MessageDigest
 import java.sql.{PreparedStatement, Timestamp}
-import java.text.{DateFormat, SimpleDateFormat}
+import java.text.SimpleDateFormat
 import java.util.Date
 
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
@@ -7,6 +9,69 @@ import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
 import scala.xml.{Elem, XML}
 
 object GetData {
+
+  def doYear(year: Int, ds: HikariDataSource): Unit = {
+    val from = s"${year}-01-01"
+    val to = s"${year}-12-31"
+
+    val element = "RR_1"
+    val hours = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23"
+    val url: String = s"http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=2&format=&from=${from}&to=${to}&stations=18700&elements=${element}&hours=${hours}&months=&username="
+
+    val cache: File = new File("../cache/" + MessageDigest.getInstance("MD5").digest(url.getBytes).map("%02X".format(_)).mkString)
+    val cacheDir: File = new File("../cache/")
+    if (!cacheDir.exists()) {
+      cacheDir.mkdirs()
+    }
+
+    if (!cache.exists()) {
+      println("downloading url " + url)
+      val data = io.Source.fromURL(url, "UTF-8")
+      val out = new PrintWriter(cache, "UTF-8")
+      data.getLines.foreach(out.println(_))
+      out.close()
+      data.close()
+      println("downloading url " + url + " done")
+    }
+
+    val data = io.Source.fromFile(cache, "UTF-8").mkString
+    val xml: Elem = XML.loadString(data)
+
+    val content = xml \\ "Envelope" \ "Body" \ "getMetDataResponse" \ "return" \ "timeStamp" \ "item"
+    val conn = ds.getConnection
+    conn.setAutoCommit(false)
+
+    var count = 0
+    val ps: PreparedStatement = conn.prepareStatement("insert into rain (measure_time, rain, quality) values (?, ?, ?)")
+    content.foreach(child => {
+      def prop(x: String) = { (child \\ x).text}
+      val date: String = prop("from")
+      var value: String = if ("-1.0".equals(prop("value"))) "0.0" else prop("value")
+      value = if ("-99999".equals(value)) "0.0" else value
+
+      val quality: String = prop("quality")
+      //println(s"${date} = quality: ${quality}, value: ${value}")
+      if (!("0,1,2,5".split(",").contains(quality)))
+        throw new RuntimeException("Unknown quality: " + quality)
+
+      val fullDate: Date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(date)
+      val bd: BigDecimal = BigDecimal(value)
+      ps.setTimestamp(1, new Timestamp(fullDate.getTime))
+      ps.setBigDecimal(2, bd.bigDecimal)
+      ps.setLong(3, quality.toLong)
+      ps.addBatch()
+      count += 1
+    })
+
+    ps.executeBatch()
+    ps.close()
+
+    println("year " + year + " with " + count + " values")
+
+    conn.commit()
+    conn.close()
+  }
+
   def main(args: Array[String]) {
     val config: HikariConfig = new HikariConfig
     config.setMaximumPoolSize(2)
@@ -17,49 +82,14 @@ object GetData {
     config.setConnectionTimeout(1500)
     val ds: HikariDataSource = new HikariDataSource(config)
 
-    /*
-    val from = "2006-01-01"
-    val to = "2006-01-01"
-    */
-    val from = "2014-01-01"
-    val to = "2014-09-08"
-    val element = "RR_1"
-    val hours = "0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23"
-    val url: String = s"http://eklima.met.no/metdata/MetDataService?invoke=getMetData&timeserietypeID=2&format=&from=${from}&to=${to}&stations=18700&elements=${element}&hours=${hours}&months=&username="
-    val data = io.Source.fromURL(url).mkString
-    val xml: Elem = XML.loadString(data)
-    println("url is ")
-    println(url)
-    //println(new PrettyPrinter(160, 1).format(xml))
-
-    val content = xml \\ "Envelope" \ "Body" \ "getMetDataResponse" \ "return" \ "timeStamp" \ "item"
     val conn = ds.getConnection
-    conn.setAutoCommit(false)
-
     conn.prepareStatement("truncate table rain").execute()
-
-    content.foreach(child => {
-      def prop(x: String) = { (child \\ x).text}
-      val date: String = prop("from")
-      var value: String = if ("-1.0".equals(prop("value"))) "0.0" else prop("value")
-      value = if ("-99999".equals(value)) "0.0" else value
-
-      val quality: String = prop("quality")
-      println(s"${date} = quality: ${quality}, value: ${value}")
-      if (!("0,1,2,5".split(",").contains(quality)))
-        throw new RuntimeException("Unknown quality: " + quality)
-
-      val fullDate: Date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(date)
-      val ps: PreparedStatement = conn.prepareStatement("insert into rain (measure_time, rain, quality) values (?, ?, ?)")
-      val bd: BigDecimal = BigDecimal(value)
-      ps.setTimestamp(1, new Timestamp(fullDate.getTime))
-      ps.setBigDecimal(2, bd.bigDecimal)
-      ps.setLong(3, quality.toLong)
-      ps.executeUpdate()
-      ps.close()
-    })
-
     conn.commit()
     conn.close()
+
+    (1950 to 2014) foreach(year => {
+      doYear(year, ds)
+    })
+
   }
 }
